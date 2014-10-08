31182a2384116e8e6c82e9c3b82628486042d200
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-10-08 16:06:21 -0400

    adding to the Score module to calculate expected values of beers based on user 1) unrated_beers method returns an array of unrated beers of current user compared to another user 2) expected_value_of_unrated_beers method returns a hash of the expected values of unrated beers of current user compared to another user 3) all_expected_values_of_unrated_beers method returns a hash of all expected values of unrated beers by all other users

--- app/models/score.rb
+++ app/models/score.rb
@@ -6,17 +6,13 @@ module Score
       o_user_id = o_user_id.to_s
       c_user_id = c_user_id.to_s
       mutual = []
-
       @dictionary[o_user_id].each do |o_beer, o_taste|
         @dictionary[c_user_id].each do |c_beer, c_taste|
-
           mutual << [o_taste, c_taste] if o_beer == c_beer
-
         end
       end
       n = mutual.length
       return 0 if n == 0
-
       o_sum = 0
       c_sum = 0
       o_sum_squared = 0
@@ -29,7 +25,6 @@ module Score
         c_sum_squared += ratings.last * ratings.last
         psum += ratings.first * ratings.last
       end
-
       numerator = psum - (o_sum * c_sum / n)
       denominator = Math.sqrt((o_sum_squared - (o_sum)**2/n) * (c_sum_squared - (c_sum)**2/n))
       if denominator == 0
@@ -37,12 +32,10 @@ module Score
       else
         numerator/denominator
       end
-
     end
 
     def top_matches(c_user_id)
-    # ordered list of people with similar tastes to specified person
-    # lets user see other users similar to that user
+    # returns top 10 users with similar tastes as another user
       c_user_id = c_user_id.to_s
       scores = []
       @dictionary.each do |o_user_id, ratings|
@@ -52,31 +45,43 @@ module Score
       scores.take(10)
     end
 
-    def user_unrated_beers
-      # find which beers the user has not rated
-      beers = []
-      Beer.all.each do |beer|
-        beers << beer.id
+    def unrated_beers(o_user_id, c_user_id)
+      # find which beers current user has not rated compared to another user
+      o_user_id = o_user_id.to_s
+      c_user_id = c_user_id.to_s
+      o_user_beers = []
+      @dictionary[o_user_id].each do |beer_id, taste|
+        o_user_beers << beer_id
       end
       c_user_beers = []
       @dictionary[c_user_id].each do |beer_id, taste|
         c_user_beers << beer_id
       end
-      beers.uniq - c_user_beers.uniq
+      o_user_beers - c_user_beers
     end
 
     def expected_values_of_unrated_beers(o_user_id, c_user_id)
-      unrated_beers = user_unrated_beers
+      o_user_id = o_user_id.to_s
+      c_user_id = c_user_id.to_s
+      beers = unrated_beers(o_user_id, c_user_id)
       sim_score = simpearson(o_user_id, c_user_id)
       expected_values = Hash.new
-      unrated_beers.each do |beer_id|
+      beers.each do |beer_id|
         expected_values[beer_id] = @dictionary[o_user_id][beer_id] * sim_score
       end
       expected_values
     end
 
     def all_expected_values_of_unrated_beers(c_user_id)
+      other_users = @dictionary.keys
+      c_user_id = c_user_id.to_s
 
+      expected_values = Hash.new
+      other_users.each do |o_user_id|
+        expected_values[o_user_id] = expected_values_of_unrated_beers(o_user_id, c_user_id)
+      end
+      expected_values
     end
+
   end
 end
